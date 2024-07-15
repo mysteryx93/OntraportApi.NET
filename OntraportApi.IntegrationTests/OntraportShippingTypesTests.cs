b9b7ef00add89e3b25a30c8592548a6fa8383701
Mike Masey <mike@huskey.uk>
2024-07-15 18:46:08 +0100

    tests: setup base tests for shipping types and tax types

--- OntraportApi.IntegrationTests/OntraportShippingTypesTests.cs
+++ OntraportApi.IntegrationTests/OntraportShippingTypesTests.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Threading.Tasks;
-using HanumanInstitute.OntraportApi.Models;
-using Xunit;
+﻿using HanumanInstitute.OntraportApi.Models;
 using Xunit.Abstractions;
 
 namespace HanumanInstitute.OntraportApi.IntegrationTests;
 
-public class OntraportShippingTypesTests : OntraportBaseDeleteTests<OntraportCoupons, ApiCoupon>
+public class OntraportShippingTypesTests : OntraportBaseDeleteTests<OntraportShippingTypes, ApiShippingType>
 {
     public OntraportShippingTypesTests(ITestOutputHelper output) :
         base(output, 1, "shippingType1")
---
+++ OntraportApi.IntegrationTests/OntraportTaxTypesTests.cs
@@ -0,0 +1,12 @@
+﻿using HanumanInstitute.OntraportApi.Models;
+using Xunit.Abstractions;
+
+namespace HanumanInstitute.OntraportApi.IntegrationTests;
+
+public class OntraportTaxTypesTests : OntraportBaseDeleteTests<OntraportTaxTypes, ApiTaxType>
+{
+    public OntraportTaxTypesTests(ITestOutputHelper output) :
+        base(output, 1, "taxType1")
+    {
+    }
+}
